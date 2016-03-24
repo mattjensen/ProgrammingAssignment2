dd1d3d09b3352d8c6e9ec69c90a59a7e6476283d
mattjensen <newsblip@gmail.com>
2016-03-24 14:50:19 -0700

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Getting and Cleaning Data
+## Assignment 2 - Lexical Scoping
+##
+## Create a special matrix holder which can cache its inverted form.
 
-## Write a short comment describing this function
+## makeCacheMatrix: creates the matrix holder. It holds original matrix,
+##     and after calling cacheSolve it will also hold a cached version of the inverse.
+
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setInvmatrix <- function(InvMatrix) m <<- InvMatrix
+  getInvmatrix <- function() m
+  list(set = set, get = get,
+       setInvmatrix = setInvmatrix,
+       getInvmatrix = getInvmatrix)
+  
 }
 
-
-## Write a short comment describing this function
+## cacheSolve: solves for and caches the inverted matrix.
+## If already cached, just return cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  m <- x$getInvmatrix()              
+  if(!is.null(m)) { # already have the inverse stored, just return it.
+    message("getting cached data")  
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setInvmatrix(m) # store the inverse in the cache
+  m
 }
+
